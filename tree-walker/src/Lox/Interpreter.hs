62ef22724dfd5d67f63b57ab6bd78242e426ecca
Empa <emanuel@empa.xyz>
2023-01-29 12:09:25 +0100

    Interpret boolean expressions

--- tree-walker/src/Lox/Interpreter.hs
+++ tree-walker/src/Lox/Interpreter.hs
@@ -13,6 +13,7 @@ emptyState = State {hadError = False}
 data InterpreterError
   = DivisionByZero
   | IncorrectType
+  | ErrorMessage String
   deriving (Show)
 
 newtype Interpreter a = Interpreter
@@ -37,23 +38,58 @@ getNumber :: Ast.Value -> Interpreter Double
 getNumber (Ast.Number d) = return d
 getNumber _ = err IncorrectType
 
-getBool :: Ast.Value -> Interpreter Bool
-getBool (Ast.Boolean b) = return b
-getBool _ = err IncorrectType
+getTruthy :: Ast.Value -> Interpreter Bool
+getTruthy Ast.Nil = return False
+getTruthy (Ast.Boolean b) = return b
+getTruthy _ = return True
+
+iNumber :: Ast.Expression -> Interpreter Double
+iNumber expr = interpretExpression expr >>= getNumber
+
+iBool :: Ast.Expression -> Interpreter Bool
+iBool expr = interpretExpression expr >>= getTruthy
 
 interpretExpression :: Ast.Expression -> Interpreter Ast.Value
 interpretExpression Ast.Literal {value} = return value
 interpretExpression Ast.Grouping {expr} = interpretExpression expr
-interpretExpression Ast.Unary {operator = Ast.Minus, rhs} =
-  Ast.Number . negate <$> (interpretExpression rhs >>= getNumber)
-interpretExpression Ast.Binary {lhs, operator, rhs} =
-  case op operator of
-    Right f ->
-      Ast.Number . (uncurry f) <$> ((,) <$> (interpretExpression lhs >>= getNumber) <*> (interpretExpression rhs >>= getNumber))
-    Left () -> err IncorrectType
+interpretExpression Ast.Unary {operator, rhs} = case operator of
+  Ast.Minus -> Ast.Number . negate <$> (interpretExpression rhs >>= getNumber)
+  Ast.Not -> Ast.Boolean . not <$> (interpretExpression rhs >>= getTruthy)
+  _ -> error "Not a unary operator"
+interpretExpression expr@Ast.Binary {operator} =
+  case operator of
+    Ast.Plus -> numberOp expr
+    Ast.Minus -> numberOp expr
+    Ast.Multiplication -> numberOp expr
+    Ast.Division -> numberOp expr
+    Ast.Equal -> boolOp expr
+    Ast.InEqual -> boolOp expr
+    Ast.Greater -> boolOp expr
+    Ast.GreaterEqual -> boolOp expr
+    Ast.Less -> boolOp expr
+    Ast.LessEqual -> boolOp expr
+    _ -> error ("Operator " ++ show operator ++ " is not a binary operator")
   where
-    op Ast.Plus = Right (+)
-    op Ast.Minus = Right (-)
-    op Ast.Multiplication = Right (*)
-    op Ast.Division = Right (/)
-    op _ = Left ()
+    numberOp Ast.Binary {lhs, operator, rhs} =
+      Ast.Number <$> (op <$> iNumber lhs <*> iNumber rhs)
+      where
+        op = case operator of
+          Ast.Plus -> (+)
+          Ast.Minus -> (-)
+          Ast.Multiplication -> (*)
+          Ast.Division -> (/)
+          _ -> error "Unexpected operator"
+    numberOp _ = error "Unexpected expression"
+
+    boolOp Ast.Binary {lhs, operator, rhs} =
+      Ast.Boolean <$> (op <$> iBool lhs <*> iBool rhs)
+      where
+        op = case operator of
+          Ast.Equal -> (==)
+          Ast.InEqual -> (/=)
+          Ast.Greater -> (>)
+          Ast.GreaterEqual -> (>=)
+          Ast.Less -> (<)
+          Ast.LessEqual -> (<=)
+          _ -> error "Unexpected operator"
+    boolOp _ = error "Unexpected expression"
