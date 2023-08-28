8a29031fc956812a6c7f2a34b5201db33984d8fe
Empa <emanuel@empa.xyz>
2023-08-28 15:44:35 +0200

    Add a simple CLI

--- bytecode/src/lox.rs
+++ bytecode/src/lox.rs
@@ -1,3 +1,21 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+struct CliArgs {
+    file: Option<String>,
+}
+
+pub fn cli() {
+    let args = CliArgs::parse();
+
+    if let Some(f) = args.file {
+        run_file(f.as_str());
+    } else {
+        repl();
+    }
+}
+
 pub fn repl() {}
 
 pub fn run_file(path: &str) {}
--- bytecode/src/main.rs
+++ bytecode/src/main.rs
@@ -7,17 +7,5 @@ mod value;
 mod vm;
 
 fn main() {
-    let mut chunk = Chunk::new();
-    let f_offset = chunk.add_constant(Value::Float(69420.));
-    chunk.add_op(Op::Constant(f_offset), 0);
-    chunk.add_op(Op::Negate, 0);
-    chunk.add_op(Op::Constant(f_offset), 0);
-    chunk.add_op(Op::Divide, 0);
-    chunk.add_op(Op::Return, 0);
-    println!("=== Chunk ===");
-    println!("{}", chunk);
-
-    let mut vm = VM::new(chunk);
-    let res = vm.run();
-    println!("{:?}", res);
+    lox::cli();
 }
